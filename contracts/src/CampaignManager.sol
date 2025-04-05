bf47cedfb8f21f8fbe42c7d9507014a89cdb249b
Pawee Tantivasdakarn <pawee@alphainnovationslab.io>
2025-04-05 20:57:04 +0800

    feat: fix code

--- contracts/src/CampaignManager.sol
+++ contracts/src/CampaignManager.sol
@@ -15,36 +15,36 @@ struct Campaign {
 contract CampaignManager {
     using SafeERC20 for IERC20;
 
-    mapping(address => uint16) public organizerCount;
-    mapping(address => mapping(uint256 => Campaign)) public campaigns;
+    mapping(address => uint256) public organizerCampaignIds;
+    mapping(uint256 => Campaign) public campaignFromId;
+    Campaign[] public campaigns;
     address acceptToken;
 
     constructor(address token) {
         acceptToken = token;
     }
 
+    function allCampaigns() public view returns (Campaign[] memory) {
+        return campaigns;
+    }
+
     function createCampaign(string memory name, uint256 targetAmount, uint256 deadline)
         external
         returns (Campaign memory newCampaign)
     {
-        uint256 count = organizerCount[msg.sender]++;
-        uint256 id = uint256(keccak256(abi.encodePacked(msg.sender, count));
-        newCampaign = Campaign({
-            id: id,
-            name: name,
-            target: targetAmount,
-            raised: 0,
-            reciever: msg.sender,
-            deadline: deadline
-        });
-        campaigns[msg.sender][id] = newCampaign;
+        uint256 id = uint256(keccak256(abi.encodePacked(msg.sender, name, targetAmount, deadline)));
+        newCampaign =
+            Campaign({id: id, name: name, target: targetAmount, raised: 0, reciever: msg.sender, deadline: deadline});
+        campaigns.push(newCampaign);
+        organizerCampaignIds[msg.sender] = id;
+        campaignFromId[id] = newCampaign;
     }
 
     function donateProxy(uint256 campaignId, address receiver, uint256 amount) external {
-        Campaign memory campaign = campaign[receiver][campignId];
-        require(campaignId == campaign.id, "Mismatched Campaign Id");
-        uint amountAfter = campaign.raised + amount
-        require(amountAfter <= campaign.target, "Target Reached")
-        IERC20(acceptToken).safeTransferFrom(msg.sender, receiver, amount);
+        uint256 receivedAmount = IERC20(acceptToken).balanceOf(address(this));
+        require(receivedAmount > 0, "No tokens received");
+        Campaign memory campaign = campaignFromId[campaignId];
+        uint256 amountAfter = campaign.raised + receivedAmount;
+        require(amountAfter < campaign.target, "Target Reached");
     }
 }
--- contracts/test/CampaignManager.t.sol
+++ contracts/test/CampaignManager.t.sol
@@ -3,22 +3,35 @@ pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {CampaignManager, Campaign} from "../src/CampaignManager.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/interfaces/IERC20.sol";
 
 contract CounterTest is Test {
     CampaignManager public campaignManager;
-    address USDT = 0xF2147b998141887Be7FA7834CCCD135e0067321a;
+    address USDC = 0x3b952c8C9C44e8Fe201e2b26F6B2200203214cfF;
+    address public constant TESTER = address(0xBEEF);
 
     function setUp() public {
         string memory url = vm.rpcUrl("zircuit_testnet");
         vm.createSelectFork(url);
-        campaignManager = new CampaignManager(USDT);
-        // counter.setNumber(0);
+        campaignManager = new CampaignManager(USDC);
     }
 
-    function test_Increment() public {
+    function test_donate() public {
         Campaign memory newCampaign = campaignManager.createCampaign("Test Campaign", 100, block.timestamp + 100);
-        console.log(newCampaign.name, newCampaign.amount, newCampaign.reciever, newCampaign.deadline);
-        console.log(newCampaign.id);
+
+        // get campaign
+        Campaign[] memory campaigns = campaignManager.allCampaigns();
+        // Campaign[] memory campaigns = campaignManager.campaigns;
+        Campaign memory campaign = campaigns[0];
+        vm.startPrank(TESTER);
+        deal(TESTER, 1 ether);
+        deal(USDC, TESTER, 100);
+        console.log(IERC20(USDC).balanceOf(TESTER));
+        campaignManager.donateProxy(campaign.id, address(0), 0);
+        vm.stopPrank();
+        // console.log(newCampaign.name, newCampaign.amount, newCampaign.reciever, newCampaign.deadline);
+        // console.log(newCampaign.id);
         // assertEq(counter.number(), 1);
     }
 
