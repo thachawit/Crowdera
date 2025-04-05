67b96e800ecd72f71b233b90ffc03835460075e9
Thawinwit <a667375@AR8040667375N1.local>
2025-04-06 03:44:31 +0700

    mock integrate + debug

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,14 @@
-import express from 'express'
 import { walletClient } from './relay-config'
 import { abi } from './contract'
-import { bytecode } from './Delegation.json'
 import { privateKeyToAccount, type Address } from 'viem/accounts'
 import type { Request, Response } from 'express';
 import type { Hex } from 'viem'
+import { parseUnits, serializeAuthorizationList } from 'viem/utils';
+const express = require('express')
+const cors = require('cors');
 const app = express();
-
+app.use(express.json());
+app.use(cors());
 const eoa = privateKeyToAccount('0x1154c5a6f0ecd6d3343d3298fa9a9e8fb11510fe0e6800c1cdc43096b9c084c7')
 
 // const  hash1 = await walletClient.writeContract({
@@ -16,29 +18,62 @@ const eoa = privateKeyToAccount('0x1154c5a6f0ecd6d3343d3298fa9a9e8fb11510fe0e680
 // })
 
 
-app.get('/designate-address', async (req: Request, res) => {
 
-  const deployedAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+interface SignedPayload {
+  signedTx: Hex
+}
+type TxProcessingError = Error & {
+  reason?:string
+}
 
-  const { contractAddress } = req.query
+interface Authorization{
+  chainId: number;
+    nonce: number;
+    r: Hex;
+    s: Hex;
+    v: number;
+    address: Address;
+    yParity?: number;
+}
 
-  const authorized = await walletClient.signAuthorization({
-    account: eoa,
-    contractAddress: deployedAddress as Address,
-  })
+interface AuthorizationFormatted{
+  chainId: number;
+    nonce: number;
+    r: Hex;
+    s: Hex;
+    v: bigint;
+    address: Address;
+    yParity?: number;
+}
 
-  console.log("Authorize", authorized)
 
-  const txHash = await walletClient.writeContract({
-    abi,
-    address: eoa.address,
-    authorizationList: [authorized],
-    functionName: 'initialize',
-  })
-  res.json({ txHash })
+app.post('/submit-tx', async (req: Request, res) => {
+  try{
+    const {authorization} = req.body 
+    console.log("bigint 1:",BigInt(1))
+    console.log(BigInt(authorization.v))
+    const authorizationFormatted: AuthorizationFormatted = {
+      chainId: authorization.chainId,
+      nonce: authorization.nonce,
+      r: authorization.r,
+      s: authorization.s,
+      v: BigInt(authorization.v),
+      address: authorization.address,
+      yParity: authorization.yParity
+    }
+    const txHash = await walletClient.writeContract({
+      abi,
+      address: eoa.address,
+      authorizationList: [authorizationFormatted],
+      functionName: 'initialize',
+    })
+    res.json({ txHash })
+  } catch (error){
+    const txError = error as TxProcessingError
+    console.error("Transaction error occured: ", txError)
+    res.status(500).json({ error: txError.message || 'Failed to boadcast transaction'})
+  }
 })
 
+app.listen(3000, () => console.log( 'API running on 3000 '))
 
-app.listen(3000, () => {
-  console.log(`started on 3000`)
-})
