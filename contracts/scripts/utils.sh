5d1cd3164da00f3173d709f40092b7dc75828160
Pawee Tantivasdakarn <pawee@alphainnovationslab.io>
2025-04-06 08:06:50 +0800

    feat: add util code

---
+++ contracts/scripts/utils.sh
@@ -0,0 +1,4 @@
+# create campaign on zircuit
+# cast send  0xf3228732b3BF34f37389109Ad8A1fc15ad8EaFcD "createCampaign(string memory name, uint256 targetAmount, uint256 deadline)" "Pet Shelter House" 200000e18 1849892539 --rpc-url https://garfield-testnet.zircuit.com/ --private-key
+# create campaign on flow
+# cast send  0xf3228732b3BF34f37389109Ad8A1fc15ad8EaFcD "createCampaign(string memory name, uint256 targetAmount, uint256 deadline)" "Pet Shelter House" 200000e18 1849892539 --rpc-url https://testnet.evm.nodes.onflow.org --private-key
--- contracts/scripts/verify.sh
+++ contracts/scripts/verify.sh
@@ -1,2 +1,9 @@
 #/bin/sh
-# forge verify-contract --verifier-url https://explorer.garfield-testnet.zircuit.com/api/contractVerifyHardhat <deployed-contract-address> <source-file>:<contract-name> --root . --etherscan-api-key <ZIRCUIT_API_KEY>
+# verify on zircuit
+# forge verify-contract --verifier-url https://explorer.garfield-testnet.zircuit.com/api/contractVerifyHardhat 0x6Cc0A879a4c59fd18553FE16CEb417652a39Afc5 CrowderaToken  --root . --etherscan-api-key FDAE1A6B127CC89DD136E778C96832DB0E
+# verify on flow
+forge verify-contract --rpc-url https://testnet.evm.nodes.onflow.org/ \
+    --verifier blockscout \
+    --verifier-url https://evm-testnet.flowscan.io/api \
+    0x2Ba069A7651904B4ABD9E8F6ff180a390530dC5F \
+    CrowderaToken
